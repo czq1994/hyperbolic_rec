7d40c3a2c29e16cd619ca3455717a962db2ec741
cuiziqiang <838556614@qq.com>
2023-02-15 21:07:20 +0800

    1

--- train_eval_hyperbolic.py
+++ train_eval_hyperbolic.py
@@ -24,7 +24,7 @@ def parse_args():
     parser.add_argument('-e', '--epoch', type=int, default=1000, help='number of epochs')
     parser.add_argument('-b', '--batch_size', type=int, default=5000, help='batch size for training')
     parser.add_argument('-dim', '--hidden_dim', type=int, default=50, help='the number of the hidden dimension')
-    parser.add_argument('-lr', '--learning_rate', type=float, default=1e-2, help='learning rate')
+    parser.add_argument('-lr', '--learning_rate', type=float, default=1e-3, help='learning rate')
     parser.add_argument('-wd', '--weight_decay', type=float, default=1e-3, help='weight decay')
     parser.add_argument('-n_neg', '--neg_samples', type=int, default=10, help='the number of negative samples')
     parser.add_argument('-dr', '--dropout_rate', type=float, default=0.5, help='the dropout probability')
