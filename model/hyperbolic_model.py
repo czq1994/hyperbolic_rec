4b4bedea67c591357c7d06c43d2a9aa47d2c3aa1
cuiziqiang <838556614@qq.com>
2023-02-15 19:15:29 +0800

    1

--- model/hyperbolic_model.py
+++ model/hyperbolic_model.py
@@ -177,7 +177,7 @@ class Model(nn.Module):
         '''
         dist = torch.zeros(y.shape[0], 1).to(self.device)
         for i in range(x.shape[0]):
-            if i % 100 == 0:
+            if i % 1000 == 0:
                 print(i)
             cur_x = x[i:i+1].repeat(y.shape[0], 1)
             cur_dist = self.sqdist(cur_x, y, c)
