c0f636823e1306a74dde4586b996be968284c5ec
cuiziqiang <838556614@qq.com>
2023-02-17 10:21:05 +0800

    1

--- manifolds/__init__.py
+++ manifolds/__init__.py
@@ -2,4 +2,4 @@ from .base import ManifoldParameter
 from .euclidean import Euclidean
 from .hyperboloid import Hyperboloid
 from .poincare import PoincareBall
-from .hyperboloid_yml_hrcf import Hyperboloid as Hyperboloid1
+# from .hyperboloid_yml_hrcf import Hyperboloid as Hyperboloid1
